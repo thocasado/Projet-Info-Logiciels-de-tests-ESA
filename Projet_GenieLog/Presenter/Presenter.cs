4c031b02d51c8ac4face24f1ed8b8dee7f34d654
emileNetter <emile@netter.fr>
2016-11-23 11:09:15 +0100

    Instanciation

--- Projet_GenieLog/Presenter/Presenter.cs
+++ Projet_GenieLog/Presenter/Presenter.cs
@@ -8,5 +8,21 @@ namespace Projet_GenieLog.Presenter
 {
     class Presenter
     {
+        private View.MyView view { get; set; }
+        private Tests tests { get; set; }
+        public Niveau difficile = Niveau.Difficile;
+        public Niveau facile = Niveau.Facile;
+
+        public Presenter()
+        {
+
+        }
+        public Presenter (View.MyView v)
+        {
+            view = v;
+            
+        }
+
+
     }
 }
