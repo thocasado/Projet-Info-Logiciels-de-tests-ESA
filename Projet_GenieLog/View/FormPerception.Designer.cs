aa39791049cf87f8afc593883de480c2889234bd
emileNetter <emile@netter.fr>
2016-11-29 14:34:33 +0100

    génère formes

--- Projet_GenieLog/View/FormPerception.Designer.cs
+++ Projet_GenieLog/View/FormPerception.Designer.cs
@@ -28,20 +28,166 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.labelA = new System.Windows.Forms.Label();
+            this.labelB = new System.Windows.Forms.Label();
+            this.labelD = new System.Windows.Forms.Label();
+            this.labelC = new System.Windows.Forms.Label();
+            this.labelE = new System.Windows.Forms.Label();
+            this.labelF = new System.Windows.Forms.Label();
+            this.labelG = new System.Windows.Forms.Label();
+            this.labelH = new System.Windows.Forms.Label();
+            this.labelI = new System.Windows.Forms.Label();
+            this.labelJ = new System.Windows.Forms.Label();
+            this.labelK = new System.Windows.Forms.Label();
+            this.labelL = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // labelA
+            // 
+            this.labelA.AutoSize = true;
+            this.labelA.Location = new System.Drawing.Point(44, 13);
+            this.labelA.Name = "labelA";
+            this.labelA.Size = new System.Drawing.Size(14, 13);
+            this.labelA.TabIndex = 0;
+            this.labelA.Text = "A";
+            // 
+            // labelB
+            // 
+            this.labelB.AutoSize = true;
+            this.labelB.Location = new System.Drawing.Point(152, 13);
+            this.labelB.Name = "labelB";
+            this.labelB.Size = new System.Drawing.Size(14, 13);
+            this.labelB.TabIndex = 0;
+            this.labelB.Text = "B";
+            // 
+            // labelD
+            // 
+            this.labelD.AutoSize = true;
+            this.labelD.Location = new System.Drawing.Point(362, 13);
+            this.labelD.Name = "labelD";
+            this.labelD.Size = new System.Drawing.Size(15, 13);
+            this.labelD.TabIndex = 0;
+            this.labelD.Text = "D";
+            // 
+            // labelC
+            // 
+            this.labelC.AutoSize = true;
+            this.labelC.Location = new System.Drawing.Point(255, 13);
+            this.labelC.Name = "labelC";
+            this.labelC.Size = new System.Drawing.Size(14, 13);
+            this.labelC.TabIndex = 0;
+            this.labelC.Text = "C";
+            // 
+            // labelE
+            // 
+            this.labelE.AutoSize = true;
+            this.labelE.Location = new System.Drawing.Point(44, 104);
+            this.labelE.Name = "labelE";
+            this.labelE.Size = new System.Drawing.Size(14, 13);
+            this.labelE.TabIndex = 0;
+            this.labelE.Text = "E";
+            // 
+            // labelF
+            // 
+            this.labelF.AutoSize = true;
+            this.labelF.Location = new System.Drawing.Point(152, 104);
+            this.labelF.Name = "labelF";
+            this.labelF.Size = new System.Drawing.Size(13, 13);
+            this.labelF.TabIndex = 0;
+            this.labelF.Text = "F";
+            // 
+            // labelG
+            // 
+            this.labelG.AutoSize = true;
+            this.labelG.Location = new System.Drawing.Point(255, 104);
+            this.labelG.Name = "labelG";
+            this.labelG.Size = new System.Drawing.Size(15, 13);
+            this.labelG.TabIndex = 0;
+            this.labelG.Text = "G";
+            // 
+            // labelH
+            // 
+            this.labelH.AutoSize = true;
+            this.labelH.Location = new System.Drawing.Point(363, 104);
+            this.labelH.Name = "labelH";
+            this.labelH.Size = new System.Drawing.Size(15, 13);
+            this.labelH.TabIndex = 0;
+            this.labelH.Text = "H";
+            // 
+            // labelI
+            // 
+            this.labelI.AutoSize = true;
+            this.labelI.Location = new System.Drawing.Point(45, 195);
+            this.labelI.Name = "labelI";
+            this.labelI.Size = new System.Drawing.Size(10, 13);
+            this.labelI.TabIndex = 0;
+            this.labelI.Text = "I";
+            // 
+            // labelJ
+            // 
+            this.labelJ.AutoSize = true;
+            this.labelJ.Location = new System.Drawing.Point(153, 195);
+            this.labelJ.Name = "labelJ";
+            this.labelJ.Size = new System.Drawing.Size(12, 13);
+            this.labelJ.TabIndex = 0;
+            this.labelJ.Text = "J";
+            // 
+            // labelK
+            // 
+            this.labelK.AutoSize = true;
+            this.labelK.Location = new System.Drawing.Point(255, 195);
+            this.labelK.Name = "labelK";
+            this.labelK.Size = new System.Drawing.Size(14, 13);
+            this.labelK.TabIndex = 0;
+            this.labelK.Text = "K";
+            // 
+            // labelL
+            // 
+            this.labelL.AutoSize = true;
+            this.labelL.Location = new System.Drawing.Point(364, 195);
+            this.labelL.Name = "labelL";
+            this.labelL.Size = new System.Drawing.Size(13, 13);
+            this.labelL.TabIndex = 0;
+            this.labelL.Text = "L";
             // 
             // FormPerception
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(512, 384);
+            this.ClientSize = new System.Drawing.Size(459, 318);
+            this.Controls.Add(this.labelC);
+            this.Controls.Add(this.labelD);
+            this.Controls.Add(this.labelB);
+            this.Controls.Add(this.labelL);
+            this.Controls.Add(this.labelH);
+            this.Controls.Add(this.labelK);
+            this.Controls.Add(this.labelG);
+            this.Controls.Add(this.labelJ);
+            this.Controls.Add(this.labelF);
+            this.Controls.Add(this.labelI);
+            this.Controls.Add(this.labelE);
+            this.Controls.Add(this.labelA);
             this.Name = "FormPerception";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Test de perception";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label labelA;
+        private System.Windows.Forms.Label labelB;
+        private System.Windows.Forms.Label labelD;
+        private System.Windows.Forms.Label labelC;
+        private System.Windows.Forms.Label labelE;
+        private System.Windows.Forms.Label labelF;
+        private System.Windows.Forms.Label labelG;
+        private System.Windows.Forms.Label labelH;
+        private System.Windows.Forms.Label labelI;
+        private System.Windows.Forms.Label labelJ;
+        private System.Windows.Forms.Label labelK;
+        private System.Windows.Forms.Label labelL;
     }
 }
--- Projet_GenieLog/View/FormPerception.cs
+++ Projet_GenieLog/View/FormPerception.cs
@@ -12,36 +12,76 @@ namespace Projet_GenieLog.View
 {
     public partial class FormPerception : Form
     {
+        SolidBrush blueBrush = new SolidBrush(Color.Blue);
+        SolidBrush yellowBrush = new SolidBrush(Color.Yellow);
 
         public FormPerception()
         {
             InitializeComponent();
         }
 
-        private void createCircle(int x, int y, int width, int height)
+        private void createCircle(int x, int y, int width, int height) // permet de creer un cercle 
         {
-            SolidBrush myBrush = new SolidBrush(Color.Blue);
+            SolidBrush blueBrush = new SolidBrush(Color.Blue);
             Graphics formGraphics = this.CreateGraphics();
-            formGraphics.FillEllipse(myBrush, new Rectangle(x, y, width, height));
-            myBrush.Dispose();
+            formGraphics.FillEllipse(blueBrush, new Rectangle(x, y, width, height));
+            blueBrush.Dispose();
             formGraphics.Dispose();
         }
 
-        private void createSquare(int x, int y, int width, int height)
+        private void createSquare(int x, int y, int width, int height) // permet de creer un carré
         {
-            SolidBrush myBrush = new SolidBrush(Color.Blue);
+            SolidBrush yellowBrush = new SolidBrush(Color.Yellow);
             Graphics formGraphics = this.CreateGraphics();
-            formGraphics.FillRectangle(myBrush, new Rectangle(x, y, width, height));
-            myBrush.Dispose();
+            formGraphics.FillRectangle(yellowBrush, new Rectangle(x, y, width, height));
+            yellowBrush.Dispose();
             formGraphics.Dispose();
+        }
+
+        private void randomizeShapes()
+        {
+            Random rnd = new Random();
+            int nbr;
+            int color;
+
+            for (int i=1; i<=3;i++)
+            {
+                for (int j=1;j<=4;j++)
+                {
+                    nbr = rnd.Next(1, 3);
+                    if(nbr ==1)
+                    {
+                        color = rnd.Next(0, 2);
+                        if(color==0)
+                        { createSquare(25 + 105 * (j - 1), 30 + 95 * (i - 1), 50, 50); }
+                        else
+                        {
+                            createSquare(25 + 105 * (j - 1), 30 + 95 * (i - 1), 50, 50);
+                        }
+                        
+                    }
+                    else
+                    {
+                        color = rnd.Next(0, 2);
+                        if (color == 0)
+                        { createCircle(25 + 105 * (j - 1), 30 + 95 * (i - 1), 50, 50); }
+                        else
+                        {
+                            createCircle(25 + 105 * (j - 1), 30 + 95 * (i - 1), 50, 50);
+                        }
+                        
+                    }
+                    
+                }
+            }
         }
 
 
         protected override void OnPaint(PaintEventArgs e)
         {
             base.OnPaint(e);
-            createSquare(30, 30, 50, 50);
-            createCircle(60, 60, 50, 50);      
+            randomizeShapes();
+            
         }
 
     }
