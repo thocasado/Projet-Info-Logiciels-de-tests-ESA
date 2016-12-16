b9b947e9e28c8f585d3335d0cc635d439d7f86c8
Thomas Casado <thocasado@ensc.fr>
2016-12-16 15:20:44 +0100

    Diagramme de classe

--- ModelingProject3Lib/Properties/AssemblyInfo.cs
+++ ModelingProject3Lib/Properties/AssemblyInfo.cs
@@ -5,22 +5,22 @@ using System.Runtime.InteropServices;
 // Les informations générales relatives à un assembly dépendent de 
 // l'ensemble d'attributs suivant. Changez les valeurs de ces attributs pour modifier les informations
 // associées à un assembly.
-[assembly: AssemblyTitle("Projet_GenieLog")]
+[assembly: AssemblyTitle("ModelingProject3Lib")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Projet_GenieLog")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCompany("Bordeaux INP - ENSC")]
+[assembly: AssemblyProduct("ModelingProject3Lib")]
+[assembly: AssemblyCopyright("Copyright © Bordeaux INP - ENSC 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 // L'affectation de la valeur false à ComVisible rend les types invisibles dans cet assembly 
-// aux composants COM.  Si vous devez accéder à un type dans cet assembly à partir de 
+// aux composants COM. Si vous devez accéder à un type dans cet assembly à partir de 
 // COM, affectez la valeur true à l'attribut ComVisible sur ce type.
 [assembly: ComVisible(false)]
 
 // Le GUID suivant est pour l'ID de la typelib si ce projet est exposé à COM
-[assembly: Guid("738fcc62-9eb4-457d-9600-50660a521879")]
+[assembly: Guid("807bdc94-686b-4833-84ae-06f7649f4308")]
 
 // Les informations de version pour un assembly se composent des quatre valeurs suivantes :
 //
@@ -34,5 +34,3 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-
-[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("TestProjet_GenieLog")]
